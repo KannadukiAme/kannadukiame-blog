3f499db3a0869d8881eac5a0cae0a0210c7a29df
KannadukiAme <misuzunagisa@gmail.com>
2023-02-12 15:24:51 +0800

    refactor code to use siteConfig

---
+++ src/utils/config.ts
@@ -0,0 +1,21 @@
+const siteConfig = {
+  title: "KannadukiAme's Blog",
+  name: '雨的小屋',
+  description:
+    '一些美少女赏析及杂谈、动画漫画游戏轻小说等原作和改编作的品鉴...',
+  nav: [
+    {
+      name: 'HOME',
+      href: '/',
+    },
+    {
+      name: 'BLOG',
+      href: '/posts',
+    },
+  ],
+  footer: {
+    copyright: 'Copyright © 2023 KannadukiAme',
+  },
+}
+
+export default siteConfig
