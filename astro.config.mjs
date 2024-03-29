a523f31f7f4a53e24a041225f3e9addebf881c2c
KannadukiAme <misuzunagisa@gmail.com>
2024-03-29 11:42:40 +0800

    add toc

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import svelte from '@astrojs/svelte'
+import remarkToc from 'remark-toc'
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [[remarkToc, { heading: '目录' }]],
+  },
   build: {
     inlineStylesheets: 'auto',
   },
