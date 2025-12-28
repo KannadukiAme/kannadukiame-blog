63492662cee21f56c4e12e9d9d2ac57d456cce73
KannadukiAme <misuzunagisa@gmail.com>
2025-12-28 19:56:06 +0800

    refactor

--- src/components/DarkMode.svelte
+++ src/components/DarkMode.svelte
@@ -1,23 +1,29 @@
 <script lang="ts">
-  let isDarkMode = localStorage.getItem("darkMode") === "true";
+  import { writable } from 'svelte/store'
+  let isDarkMode = writable<boolean | null>(null)
 
-  $: {
-    if (isDarkMode) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-    localStorage.setItem("darkMode", String(isDarkMode));
-  }
+	$effect(() => {
+		$isDarkMode = localStorage.getItem('darkMode') === 'true'
+	})
+
+	$effect(() => {
+		if ($isDarkMode) {
+			document.documentElement.setAttribute('data-mode', 'dark')
+		} else {
+			document.documentElement.setAttribute('data-mode', 'light')
+		}
+		localStorage.setItem('darkMode', String($isDarkMode))
+	})
 </script>
 
 <button
-  on:click={() => (isDarkMode = !isDarkMode)}
+  onclick={() => ($isDarkMode = !$isDarkMode)}
   aria-label="Toggle dark mode"
+  class="cursor-pointer"
 >
   <span
     class="w-5 h-5 align-sub"
-    class:icon-[ph--moon-bold]={isDarkMode}
-    class:icon-[ph--sun-bold]={!isDarkMode}
+    class:icon-[ph--moon-bold]={$isDarkMode}
+    class:icon-[ph--sun-bold]={!$isDarkMode}
   ></span>
 </button>
