efc60acf101516199987fc39688c97b888566cab
KannadukiAme <misuzunagisa@gmail.com>
2026-01-03 18:39:01 +0800

    update router & add search(not good)

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
 import tailwindcss from '@tailwindcss/vite'
+import remarkToc from 'remark-toc'
+import pagefind from 'astro-pagefind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [pagefind(), svelte()],
   vite: {
     plugins: [tailwindcss()]
+  },
+  markdown: {
+    remarkPlugins: [[remarkToc, { heading: '目录', maxDepth: 3 }]]
   }
 })
